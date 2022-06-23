9c360b7a593a1806a49f9b65e5bdb9fa5e2977c9
YuriusGB <nelf_88@mail.ru>
2022-06-23 21:23:29 +0500

    inition commit Homework 9

--- src/main/java/Lesson10/Task1/Task1.java
+++ src/main/java/Lesson10/Task1/Task1.java
@@ -7,6 +7,7 @@ public class Task1 {
     public static void main(String[] args){
         String[] massiv = {"A", "B" ,"C", "D", "E"};
         System.out.println(Arrays.toString(massiv));
+        System.out.println("меняем местами 3 и 5 элемент массива");
 
         String chengeMassiv=massiv[2];
         massiv[2]=massiv[4];
---
+++ src/main/java/Lesson9/main.java
@@ -0,0 +1,6 @@
+package Lesson9;
+
+public class main {
+    public static final int SIZE = 4;
+    private static String[][] strings = new String[SIZE][SIZE];
+}
