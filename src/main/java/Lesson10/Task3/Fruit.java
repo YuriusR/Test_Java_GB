530f31b034a659625095351fca75ee974a466b17
YuriusGB <nelf_88@mail.ru>
2022-06-26 04:20:23 +0500

    change Homework 10

--- src/main/java/Lesson10/Task3/Fruit.java
+++ src/main/java/Lesson10/Task3/Fruit.java
@@ -1,7 +1,7 @@
 package Lesson10.Task3;
 
 public abstract class Fruit {
-//    private String typeFruits;
+
     private static double weight;
 
     static enum TypeFruits {
@@ -9,17 +9,8 @@ public abstract class Fruit {
     }
 
     public Fruit(double weight) {
-//        this.typeFruits = typeFruits;
         this.weight = weight;
     }
-
-//    public String getTypeFruits() {
-//        return typeFruits;
-//    }
-//
-//    public void setTypeFruits(String typeFruits) {
-//        this.typeFruits = typeFruits;
-//    }
 
     public static double getWeight() {
         return  weight;
